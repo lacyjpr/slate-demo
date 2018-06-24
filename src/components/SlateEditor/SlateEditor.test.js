e889217ccb85687d86fe723cba23fbf53fb0aa7d
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-06-24 12:59:03 -0700

    Working SlateEditor tests

--- src/components/SlateEditor/SlateEditor.test.js
+++ src/components/SlateEditor/SlateEditor.test.js
@@ -1,55 +1,29 @@
 import React from 'react';
 //import renderer from 'react-test-renderer';
 import { shallow, mount } from 'enzyme';
-import serializer from 'enzyme-to-json/serializer';
+//import serializer from 'enzyme-to-json/serializer';
+//import { Value } from 'slate';
 
 import SlateEditor from './SlateEditor';
 
-jest.mock('slate', () => {
-  return {
-    Editor: () => <div>Heal the world!</div>,
-    Raw: {
-      deserialize: jest.fn(),
-      serialize: jest.fn(),
-    },
-  };
-});
+// jest.mock('slate', () => {
+//   return {
+//     Editor: () => <div>Heal the world!</div>,
+//     Raw: {
+//       deserialize: jest.fn(),
+//       serialize: jest.fn(),
+//     },
+//   };
+// });
 
-expect.addSnapshotSerializer(serializer);
+//expect.addSnapshotSerializer(serializer);
 
 describe('<SlateEditor />', () => {
   it('renders without crashing', () => {
     shallow(<SlateEditor />);
   });
-  // it('renders correctly', () => {
-  //   const wrapper = mount(<SlateEditor />);
-  //   expect(wrapper).toMatchSnapshot();
-  // });
+  it('renders correctly', () => {
+    const wrapper = shallow(<SlateEditor />);
+    expect(wrapper).toMatchSnapshot();
+  });
 });
-
-// import React from 'react'
-// import { mount } from 'enzyme'
-// import serializer from 'enzyme-to-json/serializer'
-// import App from './App'
-
-// jest.mock('slate', () => {
-//     return {
-//         Editor: () => (<div>Heal the world!</div>),
-//         Raw: {
-//             deserialize: jest.fn(),
-//             serialize: jest.fn()
-//         }
-//     }
-// })
-
-// // We use the enyme-to-json serializer here
-// // Note: If you put usage of serializer into jest.config.json,
-// //       it refused to work here.
-// expect.addSnapshotSerializer(serializer)
-
-// test('should render correctly', () => {
-//     const wrapper = mount(
-//         <App />
-//     )
-//     expect(wrapper).toMatchSnapshot()
-// })
