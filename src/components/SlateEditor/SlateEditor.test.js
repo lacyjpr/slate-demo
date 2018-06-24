271d6564bc712985c8d3622f44fa53279f97675b
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-06-24 13:01:00 -0700

    Clean up code

--- src/components/SlateEditor/SlateEditor.test.js
+++ src/components/SlateEditor/SlateEditor.test.js
@@ -1,22 +1,7 @@
 import React from 'react';
-//import renderer from 'react-test-renderer';
-import { shallow, mount } from 'enzyme';
-//import serializer from 'enzyme-to-json/serializer';
-//import { Value } from 'slate';
+import { shallow } from 'enzyme';
 
 import SlateEditor from './SlateEditor';
-
-// jest.mock('slate', () => {
-//   return {
-//     Editor: () => <div>Heal the world!</div>,
-//     Raw: {
-//       deserialize: jest.fn(),
-//       serialize: jest.fn(),
-//     },
-//   };
-// });
-
-//expect.addSnapshotSerializer(serializer);
 
 describe('<SlateEditor />', () => {
   it('renders without crashing', () => {
