f7329b37e2f22bab47499dfa765aa5a6f763f84d
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-06-24 08:41:16 -0700

    Add italic

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class Editor extends Component {
+  italic = () => {
+    document.execCommand('italic', false, null);
+  };
+
   bold = () => {
     document.execCommand('bold', false, null);
   };
@@ -9,6 +13,7 @@ class Editor extends Component {
     return (
       <div>
         <button onClick={this.bold}>bold</button>
+        <button onClick={this.italic}>italic</button>
         <div contentEditable="true">This text can be edited by the user.</div>
       </div>
     );
