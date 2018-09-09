8a862618c06a6bb76ba6b136dad27e915b993e0b
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-09-09 02:16:38 -0700

    Add text input with x-webkit-speech

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -15,6 +15,7 @@ class Editor extends Component {
         <button onClick={this.bold}>bold</button>
         <button onClick={this.italic}>italic</button>
         <div contentEditable="true">This text can be edited by the user.</div>
+        <input type="text" x-webkit-speech />
       </div>
     );
   }
